fbef8cea007c82f2edae1be21a31987b98304ab2
Amanda Andrade <57667417+aandradebio@users.noreply.github.com>
2022-09-25 07:06:30 -0300

    Add files via upload

---
+++ install_dependencies_Aula0.R
@@ -0,0 +1,25 @@
+#### Minicurso de Introdução a Aprendizagem de máquina para Bioinformática
+### Msc. Amanda Araújo Serrão de Andrade 
+
+## Pacotes que serão utilizados para as próximas práticas: 
+
+install.packages("seqinR")
+install.packages("kmer")
+install.packages("Caret")
+install.packages("MLeval")
+install.packages("ggplot2")
+install.packages("ggtree")
+install.packages("dplyr")
+install.packages("ape")
+install.packages("tidyverse")
+install.packages("randomForest")
+install.packages("ranger")
+install.packages("e1071")
+install.packages("adabag")
+install.packages("extraTrees")
+install.packages("ISLR")
+install.packages("tidyr")
+install.packages("caretEnsemble")
+
+# Qualquer dúvida ou problema na instalação, pode me mandar um email aandradebio@gmail.com.
+# Dúvidas específicas devem ser acompanhadas do código, print da mensagem de erro e se possível de um conjunto de dados para teste.
